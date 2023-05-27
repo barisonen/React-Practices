fd886304a457e66337ae99ca290b3e8b96804bd5
barisonen <barisonen.1996@gmail.com>
2023-05-27 23:57:26 +0300

    added action creator, folder structure, imports-exports, main page, redux store, provider etc.

--- react-redux-ts/src/App.tsx
+++ react-redux-ts/src/App.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators, State } from './state';
 
 function App() {
+  const [transitionAmount, setTransitionAmount] = useState<number>(0);
+
+  const dispatch = useDispatch();
+
+  const { depositMoney, withdrawMoney, bankrupt } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
+
+  const amount = useSelector((state: State) => state.bank);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello
-      </header>
+    <div className='App'>
+      <h1>You have: {amount}$</h1>
+      <input type="number" onChange={(e) => setTransitionAmount(e.target.valueAsNumber)}></input>
+      <br/>
+      <button onClick={() => depositMoney(transitionAmount)}>Deposit</button>
+      <button onClick={() => withdrawMoney(transitionAmount)}>Withdraw</button>
+      <br/>
+      <br/>
+      <button onClick={() => bankrupt()}>Bankrupt</button>
     </div>
   );
 }
